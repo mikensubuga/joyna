58f19c344ad49d196ebf812eee22484a4d94824d
King-Mike <nsubugamike021@gmail.com>
2019-01-13 21:08:11 +0300

    Display Posts Images

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,11 +7,11 @@
 <table class="table table-bordered">
     <tr class="info">
         <th>id</th>
+        <th>Photo</th>
+        <th>Owner</th>
+        <th>Category ID</th>
         <th>Title</th>
         <th>Body</th>
-        <th>Category ID</th>
-        <th>Owner</th>
-        <th>Photo ID</th>
         <th>created_at</th>
         <th>updated_at</th>
         
@@ -21,14 +21,21 @@
      @foreach($posts as $post)
         <tr class="active">
             <td>{{$post->id}}</td>
+
+          
+            @if ($post->photo)
+            <td> <img  width = "100" height = "100" src="{{$post->photo['file']}}" /></td>   {{--add /images before the image path works incase theres no mutator --}}
+            @else
+            <td>          
+         <img src="https://via.placeholder.com/100" alt="">
+            </td>
+
+            @endif
+            
+            <td>{{$post->user['name']}}</td> {{-- Reurns an array and should be accesed with [] instead of -> --}}
+            <td>{{$post->category_id}}</td>
             <td><a href="{{route('posts.edit', $post->id)}}">{{$post->title}}</td>
             <td>{{$post->body}}</td>
-            
-            {{-- <td> <img width = "200" src="/photos/{{$user->photo['file']}}"/></td> --}}
-            <td>{{$post->category_id}}</td>
-            <td>{{$post->user['name']}}</td> {{-- Reurns an array and should be accesed with [] instead of -> --}}
-            <td>{{$post->photo_id}}</td>
-
             <td>{{$post->created_at->diffForHumans()}}</td>
             <td>{{$post->updated_at->diffForHumans()}}</td>
         </tr>
